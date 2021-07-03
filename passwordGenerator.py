f0b65da396988bc8e8f3d1197a29f36a85461047
Ayush Sahu <ayush.cbs1@gmail.com>
2021-07-03 20:44:08 +0530

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,81 @@
+import numpy as np
+import random
+
+import smtplib
+from email.message import EmailMessage
+
+import sys
+
+
+
+#Dependencies: Install Numpy.
+
+#edits to make the code work
+#insert your emailID and password in place of the placeholders
+'''
+open your gmail
+> Click on your profile picture
+> Goto Manage Your Google Account
+> Go to Security
+> Disable 'Use your phone to sign in'
+> Disable 'Two Step Verification'
+> Turn on Less Secure App Access
+> This should work.
+This method is slightly less secure. If you don't care, go ahead.
+But if you are paranoid, watch this video by Corey Schafer to learn
+to do this in a more secure manner.
+
+I suggest that you make a new google account for this, so as to
+keep you main account clean and have a separate place for all your
+credentials.
+
+'''
+#Execute this command on your terminal
+#python3 passwordGenerator.py <website> <userID>
+
+
+def select(list, n):   
+	#randomly selects n elements from given list
+    arr=[]
+    for i in range(n):
+        arr.append(random.choice(list))
+    return arr
+
+def constSumList(Sum, n):
+    # returns a list of n positive integers with given Sum
+    counter = list(np.repeat(1,n))
+    while (sum(counter)!=Sum):
+        counter[random.choice(np.arange(n))]+=1
+    return counter
+constSumList(10,4)
+
+def passwordGenerator(n=10):
+    lowercase = list(map(chr, range(ord('a'), ord('z'))))
+    uppercase = list(map(chr, range(ord('A'), ord('Z'))))
+    numbers = list(np.arange(0,10).astype(str))
+    symbols=['#', '@', '$', '&', '*']
+
+    domain = [lowercase, uppercase, numbers,symbols]
+
+    counter = constSumList(n, 4)
+
+    password = []
+    for i in range(4):
+        password += select(domain[i], counter[i])
+    random.shuffle(password)
+    password = "".join(password)
+    return password
+
+password = passwordGenerator()
+
+msg = EmailMessage()
+msg['Subject'] = sys.argv[1] + "Credentials"
+msg['From'] = 'ayush.cbs1@gmail.com'
+msg['To'] = 'loneshadowphysx@gmail.com'
+msg.set_content('Your userID and password for {}:\n {} {}'.format(sys.argv[1], sys.argv[2], password ))
+print('Your UserID and password for {}: {}\n {}'.format(sys.argv[1], sys.argv[2],password))
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    
+    smtp.login('ayush.cbs1@gmail.com', 'overwatchogl20')
+    smtp.send_message(msg)
